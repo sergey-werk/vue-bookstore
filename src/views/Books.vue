4484078833dbd4314495d5384ba562dbf52df59f
Sergey Ryzhikov <sergey-werk@ya.ru>
2020-09-29 09:51:41 +0000

    fix naming consistency, format

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -1,47 +1,41 @@
 <template>
-<div class="items-catalog container-fluid content-row">
-  <h1>Books Catalog</h1>
-  <p>
-    <b-form-checkbox v-model="listView" name="check-button" switch>
-      List view
-    </b-form-checkbox>
-  </p>
-  <div class="row" v-if="!listView">
-    <div
-      v-for="item in items"
-      :key="item.id"
-      class="item-card col-xs-12 col-sm-6 col-md-4 d-flex"
-    >
-      <BookCard
-      :item="item"
-      class="w-100 mb-4"
-      ></BookCard>
-    </div>
-  </div>
+  <div class="items-catalog container-fluid content-row">
+    <h1>Books Catalog</h1>
+    <p>
+      <b-form-checkbox v-model="listView" name="check-button" switch>
+        List view
+      </b-form-checkbox>
+    </p>
 
-  <table class="table" v-else-if="listView">
-    <thead>
-      <tr>
-        <th>Артикул</th>
-        <th>Название</th>
-        <th>Цена</th>
-      </tr>
-    </thead>
-    <tbody>
-      <BookRow
-        :item="item"
+    <div class="row" v-if="!listView">
+      <div
         v-for="item in items"
         :key="item.id"
-      ></BookRow>
-    </tbody>
-  </table>
-</div>
+        class="item-card col-xs-12 col-sm-6 col-md-4 d-flex"
+      >
+        <BooksCard :item="item" class="w-100 mb-4"></BooksCard>
+      </div>
+    </div>
+
+    <table class="table" v-else-if="listView">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Title</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <BooksRow :item="item" v-for="item in items" :key="item.id"></BooksRow>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
-import BookRow from './BooksRow.vue';
-import BookCard from './BooksCard.vue';
+import BooksRow from './BooksRow.vue';
+import BooksCard from './BooksCard.vue';
 
 export default {
   data() {
@@ -50,8 +44,8 @@ export default {
     };
   },
   components: {
-    BookRow,
-    BookCard,
+    BooksRow,
+    BooksCard,
   },
   computed: {
     ...mapState('catalog', ['items']),
@@ -65,7 +59,7 @@ export default {
 <style scoped>
 @media (min-width: 1100px) {
   .item-card {
-    flex: 0 0 350px!important;
+    flex: 0 0 350px !important;
   }
 }
 </style>
