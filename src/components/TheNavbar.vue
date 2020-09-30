270de5179b8327cfb35c10b59583d22217beca55
Sergey Ryzhikov <sergey-werk@ya.ru>
2020-09-30 23:12:21 +0000

    fix absolute path in urls

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -5,9 +5,9 @@
     <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
     <b-collapse id="nav-text-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item :to="'books'">Books</b-nav-item>
-        <b-nav-item :to="'authors'">Authors</b-nav-item>
-        <b-nav-item :to="'about'">About</b-nav-item>
+        <b-nav-item :to="'/books'">Books</b-nav-item>
+        <b-nav-item :to="'/authors'">Authors</b-nav-item>
+        <b-nav-item :to="'/about'">About</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
 </b-navbar>
