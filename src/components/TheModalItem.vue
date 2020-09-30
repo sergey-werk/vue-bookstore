a58a356e66fb61cc9de361c02fe9fc20889cf1a1
Sergey Ryzhikov <sergey-werk@ya.ru>
2020-09-30 21:58:30 +0000

    modal item

---
+++ src/components/TheModalItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <b-modal v-bind="$attrs" scrollable ok-only centered>
+    <slot name="body"> Default body </slot>
+  </b-modal>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: ['item'],
+};
+</script>
