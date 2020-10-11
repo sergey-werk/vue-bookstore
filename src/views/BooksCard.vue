33ca18775f2bedce3b04e01df9d9ff703b36b357
Sergey Ryzhikov <sergey-werk@ya.ru>
2020-10-12 01:30:24 +0300

    feature: rating

--- src/views/BooksCard.vue
+++ src/views/BooksCard.vue
@@ -1,5 +1,12 @@
 <template>
   <b-card class="books-card" :img-src="item.image | base_url" img-alt="Book cover" img-top>
+    <b-form-rating class="book-card-rating" inline readonly
+      :value="item.rating"
+      style=""
+      no-border
+      variant="warning"
+      v-if="item.rating > 1"
+      />
     <b-card-title>
       <router-link
         :to="{ name: 'Book', params: { id: item.id }}"
@@ -8,15 +15,21 @@
         @click.native.prevent="onClick"
         >
         {{ item.title }}
+        {{ item.rating }}
       </router-link>
     </b-card-title>
     <b-card-sub-title>
       {{ item.subtitle }}
     </b-card-sub-title>
-    <b-card-text> </b-card-text>
+    <b-card-text />
     <template v-slot:footer>
-      <small class="text-muted">ISBN: {{ item.isbn13 }}</small> -
-      {{ item.price }}
+        <div class="d-flex">
+        <div>
+          <small class="text-muted">ISBN-13: {{ item.isbn13 }}</small> <br/>
+          <small class="text-muted">ISBN-10: {{ item.isbn10 }}</small>
+        </div>
+        <div class="ml-auto align-self-center">{{ item.price }}</div>
+        </div>
     </template>
   </b-card>
 </template>
@@ -38,11 +51,18 @@ export default {
 .card-img-top {
   max-height: 350px;
   margin-bottom: -20px;
+  margin-top: 10px;
   object-fit: scale-down;
 }
 
 .card-title a {
   color: darkslategrey;
   text-decoration: none;
+}
+
+.book-card-rating {
+  background-color:transparent;
+  position: absolute;
+  top: 0; left: 0;
 }
 </style>
